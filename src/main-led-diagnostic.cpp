f0942959492d03e7b933d2c181f61812abe5f253
Carl Raymond <carl@orcharddev.com>
2023-08-18 08:21:59 -0400

    Better LED test

--- src/main-led-diagnostic.cpp
+++ src/main-led-diagnostic.cpp
@@ -1,9 +1,5 @@
 #include <Arduino.h>
 #include <FastLED.h>
-#include "innards.h"
-
-// How many leds in your strip?
-#define NUM_LEDS 335
 
 // For led chips like WS2812, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
@@ -12,47 +8,50 @@
 #define DATA_PIN 4
 #define DELAY 50
 
-// Define the array of leds
-CRGB leds[NUM_LEDS];
+// Define the array of pixels
+const int NumPixels = 360;
+CRGB pixels[NumPixels];
+
+// Color wash offset -- the first pixels are
+// in the nose, and behind a red lens.
+const uint8_t offset = 16;
 
 void setup() {
   // put your setup code here, to run once:
-  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is typical
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(pixels, NumPixels);  // GRB ordering is typical
 }
 
-void fill(Organ org, CRGB::HTMLColorCode color, unsigned long delay_ms) {
-  for (int i=org.start; i < org.start+org.len;  i++) {
-    if (i==45) continue;
-    leds[i] = color;
-    FastLED.show();
-    delay(delay_ms);
-  }
-}
+const uint16_t step_forward = 17;
+const uint16_t step_backward = -13;
 
-void blink(Organ org, CRGB::HTMLColorCode color, unsigned long delay_ms) {
-  for (int i=org.start; i < org.start+org.len;  i++) {
-    if (i==45) continue;
-    leds[i] = color;
-    FastLED.show();
-    delay(delay_ms);
-    leds[i] = CRGB::Black;
-  }
-}
-
-void goround(CRGB::HTMLColorCode color) {
-    for (int k=0;  k<ORGAN_COUNT;  k++) {
-      blink(BodyMap[k], color, 50);
-      fill(BodyMap[k], color, 25);
-      fill(BodyMap[k], CRGB::Black, 25);
-    }
-}
+// Time counter for color wash.
+uint16_t time_forward = 0;
+// Time counter for contrast pixels.
+uint16_t time_backward = 0;
+// Time counter for nose ring
+uint16_t time_nose = 0;
+// Index of reverse pixels (counts mod 16)
+uint16_t reverse_index = 0;
 
 void loop() {
-  goround(CRGB::Red);
-  goround(CRGB::Green);
-  goround(CRGB::Blue);
-  goround(CRGB::Yellow);
-  goround(CRGB::Purple);
-  goround(CRGB::Turquoise);
-  goround(CRGB::White);
+    time_forward += step_forward;
+    time_backward += step_backward;
+
+    // Color the wash pixels
+    uint16_t start = time_forward;
+    for (int i = 0; i < NumPixels;  i++) {
+      uint16_t hue = start + (i << 6);
+      uint8_t hueb = hue >> 8;
+      pixels[i].setHSV(hueb, 192, 128);
+    }
+
+    // Overlay the reverse
+    start = time_backward;
+    for (int i = reverse_index; i < NumPixels; i+=16) {
+      uint16_t hue = start + (i << 6);
+      uint8_t hueb = hue >> 8;
+      pixels[i].setHSV(hueb, 128, 83);
+    }
+
+    FastLED.show();
 }
