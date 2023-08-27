5fc347041c2d70bc224a03859beda018c64a1ef4
Carl Raymond <carl@orcharddev.com>
2023-08-27 15:51:23 -0400

    Moar debugs

--- src/main-led-diagnostic.cpp
+++ src/main-led-diagnostic.cpp
@@ -1,10 +1,6 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-// For led chips like WS2812, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-// Clock pin only needed for SPI based chipsets when not using hardware SPI
 #define DATA_PIN 4
 #define DELAY 50
 
@@ -18,7 +14,7 @@ CRGB pixels[total_pixels];
 // the one we animate. Pixel positions are computed from 0 -- chaser_pixels - 1,
 // and the nose_offset is added when addressing the strip.
 
-const uint8_t slow_factor = 5;
+const uint8_t slow_factor = 3;
 
 struct Layer {
   uint16_t hue;                 // Color counter
@@ -31,18 +27,18 @@ struct Layer {
   const uint8_t value;
 };
 
-const uint8_t layer_count = 4;
+const uint8_t layer_count = 2;
 Layer Layers[] = {
-  { 0,      17,   1,   1,   0,    0,  100,   96 },
-  { 0,      11,   4,  67,   0,    3,  128,  128 },
-  { 21845, -28,   8,  33,   0,    5,  192,  128 },
-  { 43690, -45,   3,  130,  0,  -23,  240,  240 }
+  {     0,   17,   1,    1,   0,    0,  100,   96 },
+  {     0,   28,   8,   33,   0,    5,  192,  128 },
+  {     0,   11,   5,   67,   0,   -3,  240,  240 },
+  { 43690,  -45,   3,  130,   0,  -23,  240,  240 }
 };
 
-// Counts down mod slow_factor to produce a slower clock
+// Counts down modulo slow_factor to produce a slower clock
 uint8_t slow_clock = slow_factor;
 
-// Counts mod 16, slowed down by slow_factor
+// Counts nose pixels, slowed down by slow_factor
 uint8_t nose_counter = 0;
 
 
@@ -65,47 +61,41 @@ void loop() {
 
     if (--slow_clock == 0) {
       slow_clock = slow_factor;
-      
+
       // Advance the nose pixel
       pixels[nose_counter] = CRGB::Black;
       nose_counter = (nose_counter-1) & 0xf;
       pixels[nose_counter] = CRGB::Red;
-      //Serial.println(nose_counter);
-
-      // Advance .offset by .offset_step
-      for (uint8_t l=0;  l < layer_count;  l++) {
-        Layers[l].offset += Layers[l].offset_step;
-      }
     }
 
     // Animate each layer
     for (uint8_t l=0;  l<layer_count;  l++) {
+      // Advance .offset by .offset_step
+      Layers[l].offset += Layers[l].offset_step;
+
       if (Layers[l].stride == 1) {
         // Background layer: color every pixel
         for (uint16_t i = 0;  i < chaser_pixels;  i++) {
-          uint16_t hue = Layers[l].hue + (i << 6);
-          uint8_t hueb = hue >> 8;
-          pixels[i + nose_pixels].setHSV(hueb, Layers[l].saturation, Layers[l].value);
+          //uint16_t hue = Layers[l].hue + (i << 6);
+          //uint8_t hueb = hue >> 8;
+          //pixels[i + nose_pixels].setHSV(hueb, Layers[l].saturation, Layers[l].value);
+          pixels[i + nose_pixels] = CRGB::Black;
         }
       }
       else {
         // Overlay layer: color .run pixels out of every .stride with offset
         uint16_t offset = (Layers[l].offset >> 6) % Layers[l].stride;
-
-        for (uint16_t i = 0;  i < chaser_pixels;  i+=Layers[l].stride) {
-          for (uint8_t r = 0;  r < Layers[l].run;  r++) {
-            uint16_t hue = Layers[l].hue + ((i+r) << 6);
+        for (uint16_t start = offset;  start < chaser_pixels;  start += Layers[l].stride) {
+          for (uint16_t r = 0;  r < Layers[l].run;  r++) {
+            uint16_t hue = Layers[l].hue + ((start+r) << 6);
             uint8_t hueb = hue >> 8;
-            uint16_t pos = offset + i + r;
-            if (pos > chaser_pixels) { pos -= chaser_pixels; }
-            if (pos < 0) {
-              Serial.print("pos < 0: ");
-              Serial.println(pos);
+            uint16_t pos = start + r;
+            if (pos > chaser_pixels) {
+              break;
             }
             pixels[pos + nose_pixels].setHSV(hueb, Layers[l].saturation, Layers[l].value);
           }
         }
-        Serial.println();
       }
     }
 
